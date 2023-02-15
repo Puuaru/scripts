b62be458474d522e0806b48b257770aa623c17d5
Puuaru <honghuizhang4w@gmail.com>
2023-02-15 23:59:23 +0800

    feat: add bluetooth manager

---
+++ bluetooth-manager.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+bluetoothctl power on
+
+connect="connect"
+newConnection="new connection"
+disconnect="disconnect"
+
+options="$connect\n$new connection\n$disconnect"
+
+mode="$(echo -e "$options" | rofi -i -p 'bluetooth manager' -dmenu)"
+case $mode in
+  $connect)
+    selection=($(bluetoothctl devices | awk '{print $2,$3}' | rofi -i -p 'Connect:' -dmenu))
+    bluetoothctl connect ${selection[0]} && notify-send "bluetooth connect to ${selection[1]}"
+    ;;
+  $newConnection)
+    $TERM -e bluetoothctl
+    ;;
+  $disconnect)
+    selection=($(bluetoothctl devices | awk '{print $2,$3}' | rofi -i -p 'Disconnect:' -dmenu))
+    bluetoothctl disconnect ${selection[0]} && notify-send "bluetooth disconnect from ${selection[1]}"
+    ;;
+esac
